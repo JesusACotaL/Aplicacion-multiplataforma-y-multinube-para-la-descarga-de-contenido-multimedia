a1fe73b31bf3c944620d04d82b1657c66c9206c4
DiegoServinHdn <diegoservinhdn@gmail.com>
2022-05-16 16:31:40 -0500

    fix get manga chapters

--- mangaback/lambdas/manga_scrapers/src/apps/get_manga_chapters.py
+++ mangaback/lambdas/manga_scrapers/src/apps/get_manga_chapters.py
@@ -16,8 +16,11 @@ def scraper_handler(event: dict, context: str) -> dict:
     print(context)
 
     request_body = event.get("body")
-    chapters_url = request_body.get("url")
-
-    scraper = ManganeloScraper()
-
-    return scraper.crawl_chapters(chapters_url)
+    chapter_url = request_body.get("url")
+    scraper = ManganeloScraper() 
+    chapters_raw = scraper.crawl_chapters(chapter_url)
+    response = []
+    for key, value  in chapters_raw.items():
+        response.append({"Nombre": key, "Url": value})
+    
+    return response

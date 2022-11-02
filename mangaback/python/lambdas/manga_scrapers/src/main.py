4781138ee30236e231c376ba22f2b4f8a089f90b
Diego Servin <diego.servin@xaldigital.com>
2022-11-02 17:26:51 -0600

    initial commit

---
+++ mangaback/python/lambdas/manga_scrapers/src/main.py
@@ -0,0 +1,23 @@
+from functools import wraps
+from apps.get_manga_chapters import scraper_handler
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        # first item in the args, ie `args[0]` is `self`
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
+
+
+test_json = {
+    "body": {
+        "url": "https://chapmanganelo.com/manga-aa88620"
+    }
+}
+
+scraper_handler(test_json, 'test')

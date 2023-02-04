7ec68c70f9546d5d93fbeebb2d666be4da02070a
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-02-04 01:11:45 -0600

    Acepta todo tipo de imagenes soportadas por jsPDF, textarea para URLS
    Se puede poner el link de las imagenes a cargar.

--- pdf-desde-front/src/app/app.component.ts
+++ pdf-desde-front/src/app/app.component.ts
@@ -8,15 +8,15 @@ import { jsPDF } from "jspdf";
 })
 export class AppComponent {
   title = 'pdf-desde-front';
+  fuentes = "";
   imgs: Array<HTMLImageElement> = [];
   constructor() {}
   ngOnInit() {}
 
-  cargarImagenes() {
-    let fuentes = [
-      "assets/imagenesPrueba/output.jpg",
-      "assets/imagenesPrueba/output2.jpg"
-    ];
+  cargarImagenes(urls: string) {
+    this.imgs = [];
+    let fuentes = [];
+    fuentes = urls.split("\n");
     for (const fuente of fuentes) {
       const img = new Image();
       img.src = fuente;
@@ -32,7 +32,7 @@ export class AppComponent {
     });
     for (const [i,img] of imagenes.entries()) {
       if(i > 0) pdf.addPage();
-      pdf.addImage(img, 'JPG', 0, 0, pdf.internal.pageSize.getWidth(),pdf.internal.pageSize.getHeight());
+      pdf.addImage(img, 0, 0, pdf.internal.pageSize.getWidth(),pdf.internal.pageSize.getHeight());
     }
     pdf.save("queso.pdf");
   }

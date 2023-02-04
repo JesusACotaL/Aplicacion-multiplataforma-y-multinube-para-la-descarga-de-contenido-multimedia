fb64d54bf0e5d1d326445a1e1520937b930bf4e4
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-02-03 21:49:54 -0600

    Prototipo funcionando (generar imagenes y generar un pdf de las mismas)
    Notas:
    jspdf puede no funcionar igual segun el dispositivo, habria que probarlo en Android y otros.
    Las imagenes se considera que tienen una relacion de aspecto de una hoja A4 de pdf.
    Si son diferentes, se achataran. Todo depende de la relacion de aspecto de la imagen.
    
    Las imagenes fueron generadas por una IA (https://boredhumans.com/text-to-image.php)
    y no tienen ningun tipo de derecho de autor.

--- pdf-desde-front/src/app/app.component.ts
+++ pdf-desde-front/src/app/app.component.ts
@@ -8,16 +8,32 @@ import { jsPDF } from "jspdf";
 })
 export class AppComponent {
   title = 'pdf-desde-front';
+  imgs: Array<HTMLImageElement> = [];
   constructor() {}
   ngOnInit() {}
 
-  generarPDF() {
-    const doc = new jsPDF({
-      orientation: "landscape",
-      unit: "in",
-      format: [4, 2]
+  cargarImagenes() {
+    let fuentes = [
+      "assets/imagenesPrueba/output.jpg",
+      "assets/imagenesPrueba/output2.jpg"
+    ];
+    for (const fuente of fuentes) {
+      const img = new Image();
+      img.src = fuente;
+      this.imgs.push(img);
+    }
+  }
+
+  generarPDF(imagenes: Array<HTMLImageElement>) {
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: 'in',
+      format: 'a4'
     });
-    doc.text("Hello world!", 10, 10);
-    doc.save("a4.pdf");
+    for (const [i,img] of imagenes.entries()) {
+      if(i > 0) pdf.addPage();
+      pdf.addImage(img, 'JPG', 0, 0, pdf.internal.pageSize.getWidth(),pdf.internal.pageSize.getHeight());
+    }
+    pdf.save("queso.pdf");
   }
 }

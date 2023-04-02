cbcdb45b784625c1fb2bbd6c50e87af835dce91b
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-04-01 18:54:49 -0600

    Implementacion de query en URL en pagina buscar
    Por hacer:
    - Que se pueda volver a buscar desde la misma pagina
    - Cuando no se encuentran resultados

--- mangafront/src/app/buscar/buscar.component.ts
+++ mangafront/src/app/buscar/buscar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MangaApiService } from '../services/manga-api.service';
 
 @Component({
   selector: 'app-buscar',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buscar.component.css']
 })
 export class BuscarComponent implements OnInit {
+  manga = '';
+  mangasEncontrados = [
+    {
+      name: '',
+      author: '',
+      image_url: '',
+      chapters_url: '',
+      stars: ''
+    }
+  ];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private mangaAPI: MangaApiService) { }
 
   ngOnInit(): void {
+    // Obtener nombre de manga
+    this.route.params.subscribe( (parametros) => {
+      console.log(parametros);
+      this.manga = parametros['name'];
+      // Buscar manga en API
+      this.mangaAPI.buscarManga(this.manga).subscribe( (mangas) => {
+        console.log('Mangas encontrados:');
+        console.log(mangas);
+        this.mangasEncontrados = mangas;
+      });
+    });
   }
 
 }
--- mangafront/src/app/home/home.component.ts
+++ mangafront/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  // Como funcionan los template-driven forms: https://angular.io/guide/forms#summary
+  manga = '';
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  buscar() {
+    this.router.navigate(['/buscar',this.manga]);
   }
 
 }

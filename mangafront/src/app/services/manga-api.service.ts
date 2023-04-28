b84abf1124b97e16abad4c559b6426b84f094ac4
DeadZombie14 <43389808+DeadZombie14@users.noreply.github.com>
2023-04-28 02:00:57 -0600

    Algunos cambios en el diseño
    - Los mangas ya no se centran, se acomodan a la izquierda
    - Agregado un boton para cambiar de fuentes a futuro

--- mangafront/src/app/services/manga-api.service.ts
+++ mangafront/src/app/services/manga-api.service.ts
@@ -12,7 +12,7 @@ export class MangaApiService {
 
   private manganelo = 'https://m.manganelo.com';
   private backend = 'https://5t9ckx5fk5.execute-api.us-west-1.amazonaws.com/final';
-  private datosPrueba = false;
+  private datosPrueba = true;
 
   obtenerBusquedasPopulares() {
     return this.http.get('assets/datosPrueba/get-popular-search.json');

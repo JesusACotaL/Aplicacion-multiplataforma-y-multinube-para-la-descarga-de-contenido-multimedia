4a041d91c4b02a12c96bba4de2d7b467676ad765
elderdeveloper15 <jesusangel16300077@gmail.com>
2023-06-22 17:06:29 -0600

    Base de datos Firestore usuarios

--- mangafront/src/app/services/user.service.ts
+++ mangafront/src/app/services/user.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from "@angular/core";
 import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, GoogleAuthProvider, browserSessionPersistence} from "@angular/fire/auth";
 import { User, onAuthStateChanged, setPersistence } from 'firebase/auth'; // Importar User de firebase/auth
 import { FirebaseApp } from '@angular/fire/app';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+
 
 @Injectable({
     providedIn:'root'
@@ -10,12 +13,16 @@ export class UserService{
 
     currentUser = this.auth.currentUser;   
 
-    constructor(private auth:Auth){
+    constructor(private auth:Auth,private firestore: AngularFirestore){
         setPersistence(this.auth, browserSessionPersistence);
     }
 
-    register({email,password}: any){
-        return createUserWithEmailAndPassword(this.auth,email,password);
+    register({email, password}: any){
+        return createUserWithEmailAndPassword(this.auth, email, password)
+          .then(response => {
+            this.saveUserToFirestore(response.user);
+            return response;
+          });
     }
 
     login({email,password}: any){
@@ -26,10 +33,14 @@ export class UserService{
         return signOut(this.auth);
     }
 
-    loginWithGoogle(){
-        return signInWithPopup(this.auth,new GoogleAuthProvider());
-    }
-
+    loginWithGoogle() {
+        return signInWithPopup(this.auth, new GoogleAuthProvider())
+          .then(response => {
+            this.saveUserToFirestore(response.user);
+            return response;
+          });
+      }
+    
     userAuthState(){
         let flag: boolean = false;
         var user = this.auth.currentUser;
@@ -45,6 +56,15 @@ export class UserService{
 
     getAuth(){
         return this.auth;
+    }
+
+    saveUserToFirestore(user: User) {
+        return this.firestore.collection('users').doc(user.uid).set({
+            email: user.email,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            uid: user.uid
+        });
     }
 
 }

2090f01f1b8666758cdc410806ae5555b460cf4a
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-06-27 19:47:06 -0600

    Habilitar CORS en API
    Al parecer no se habilitan por defecto? Asi que se instalo el plugin flask-cors y listo
    https://www.geeksforgeeks.org/how-to-install-flask-cors-in-python/

--- mangaback/flaskBackend/main.py
+++ mangaback/flaskBackend/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 from myanimelistScrapper import scrapManga, searchMangaOnline
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():

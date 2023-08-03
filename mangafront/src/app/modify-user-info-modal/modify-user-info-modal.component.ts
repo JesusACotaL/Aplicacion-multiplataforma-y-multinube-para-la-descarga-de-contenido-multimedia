1d5ffe3f625f8f73636e4868a22b5bb16298756b
elderdeveloper15 <jesusangel16300077@gmail.com>
2023-08-03 17:52:04 -0600

    Profile Picture XD

--- mangafront/src/app/modify-user-info-modal/modify-user-info-modal.component.ts
+++ mangafront/src/app/modify-user-info-modal/modify-user-info-modal.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
 import * as bootstrap from 'bootstrap';
 
 import { UserService } from '../services/user.service';
 import { User } from 'firebase/auth'; // Importar User de firebase/auth
+
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+interface UserDocument {
+  photoURL: string;
+  // Otras propiedades que puedas tener en el documento
+}
 
 @Component({
   selector: 'app-modify-user-info-modal',
@@ -10,10 +17,13 @@ import { User } from 'firebase/auth'; // Importar User de firebase/auth
   styleUrls: ['./modify-user-info-modal.component.css']
 })
 export class ModifyUserInfoModalComponent implements OnInit {
+  @ViewChild("fileInput") fileInput!: ElementRef<HTMLInputElement>;
   accountModal!: bootstrap.Modal;
   user: User | null = null;
+  profilePicture= '';
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService,private firestore: AngularFirestore) { }
+
 
   ngOnInit(): void {
     this.accountModal = new bootstrap.Modal('#accountModal', {keyboard: false});
@@ -22,11 +32,54 @@ export class ModifyUserInfoModalComponent implements OnInit {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
-        this.user = user;        
+        this.user = user;   
+        
+        this.firestore.collection('users').doc(this.user.uid).get()
+            .subscribe((doc) => {
+              if (doc.exists) {
+                const userData = doc.data() as UserDocument;
+                this.profilePicture = userData.photoURL;
+              } else {
+                console.log("El documento no existe.");
+              }
+            });
       } else {
         // User is signed out
       }
     });
+  }
+
+  openFileInput() {
+    // Abre el cuadro de diálogo de carga de archivos al hacer clic en la imagen del usuario
+    this.fileInput.nativeElement.click();
+  }
+
+  reloadPage(){
+    window.location.reload();
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files![0];
+    if (file) {
+      // Lógica para cargar la imagen al servicio de Firebase
+      this.userService.uploadPhoto(file).subscribe(
+        (url) => {
+          // Actualiza la URL de la imagen en profilePicture
+          this.profilePicture = url;
+
+          const docRef = this.firestore.collection('users').doc(this.user!.uid);
+          const newPic = {
+            photoURL: this.profilePicture
+          };
+          docRef.update(newPic).then(()=>{});
+
+        },
+        (error) => {
+          console.error("Error al cargar la imagen:", error);
+          // Lógica adicional de manejo de errores si es necesario
+        }
+      );
+    }
   }
 
   mostrar() {

a62239d76f877a98122132285e39fde5d6fec643
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-08-06 19:29:03 -0600

    Permitir que se inicie en red local, quitar cursiva de /home

--- mangaback/backendAPIs/startALL.cmd
+++ mangaback/backendAPIs/startALL.cmd
@@ -1,23 +1,23 @@
 call .venv/Scripts/activate
 cd mainAPI
-start flask --app main run --debug --port 5000
+start flask --app main run --debug --port 5000 --host=0.0.0.0
 cd ..
 
 cd mangaInfoAPIs
 cd myAnimeList
-start flask --app myAnimeList run --debug --port 5001
+start flask --app myAnimeList run --debug --port 5001 --host=0.0.0.0
 cd ..
 cd ..
 
 cd mangaAPIs
 cd manganelo
-start flask --app manganelo run --debug --port 5002
+start flask --app manganelo run --debug --port 5002 --host=0.0.0.0
 cd ..
 cd mangakakalottv
-start flask --app mangakakalottv run --debug --port 5003
+start flask --app mangakakalottv run --debug --port 5003 --host=0.0.0.0
 cd ..
 cd mangakakalotcom
-start flask --app mangakakalotcom run --debug --port 5004
+start flask --app mangakakalotcom run --debug --port 5004 --host=0.0.0.0
 cd ..
 cd ..
 

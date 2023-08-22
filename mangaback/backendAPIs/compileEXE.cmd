3a7601077a25fe2525f360ca85809438f0de280a
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-08-22 02:01:15 -0600

    Compilar exe funcionando (yey)

--- mangaback/backendAPIs/compileEXE.cmd
+++ mangaback/backendAPIs/compileEXE.cmd
@@ -1,4 +1,4 @@
 call .venv/Scripts/activate
 cd mainAPI
-pyinstaller --add-data "firebase-credentials.json;." --noconfirm productionEXE.py
+pyinstaller --noconfirm --add-data "mangafront;mangafront" --add-data "firebase-credentials.json;." productionEXE.py --icon="./mangafront/favicon.ico"
 pause
--- mangaback/backendAPIs/mainAPI/productionEXE.py
+++ mangaback/backendAPIs/mainAPI/productionEXE.py
@@ -3,7 +3,7 @@ MAIN API ENDPOINT for Aplicacion multiplataforma y multinube para la descarga de
 pip install -r requirements.txt
 pip freeze > requirements.txt
 """
-from main import app
+from main import *
 import webbrowser
 from waitress import serve
 import time
@@ -17,8 +17,6 @@ def find_free_port():
         return s.getsockname()[1]
 port = find_free_port()
 
-port = 8080 # For whatever reason, we get 404 unless in default port?
-print('Serving on 127.0.0.1:'+str(port))
-webbrowser.open('http://127.0.0.1:'+str(port),new=0, autoraise=True)
-#app.run(host='127.0.0.1', port=port, debug=True)
-serve(app, host='127.0.0.1', port=port)
+print('Serving on 0.0.0.0:'+str(port))
+webbrowser.open('http://0.0.0.0:'+str(port),new=0, autoraise=True)
+serve(app, host='0.0.0.0', port=port)

e5a857875f7fd2c26cb0d1079d49521d6d0db5e0
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-08-23 15:03:04 -0600

    Compilador: compilar automaticamente todos los modulos y guardar con un for

--- mangaback/backendAPIs/compileAppStarter.cmd
+++ mangaback/backendAPIs/compileAppStarter.cmd
@@ -1,28 +1,28 @@
-call .venv/Scripts/activate
-cd mainAPI
+CALL .venv/Scripts/activate
+
+SET root=%CD%
+SET appstarter=%CD%\appStarter
+
+CD %root%\mainAPI
+ECHO "Compiling mainAPI..."
 pyinstaller executable.py --noconfirm --add-data "mangafront;mangafront" --add-data "firebase-credentials.json;." --distpath ../appStarter/mainAPI  --workpath ../appStarter/mainAPI/build
-cd ..
 
-cd sourceAPIs
-cd mangaInfoAPIs
-cd myAnimeList
-pyinstaller executable.py --noconfirm --distpath ../../../appStarter/mangaInfoAPIs/myAnimeList  --workpath ../../../appStarter/mangaInfoAPIs/myAnimeList/build
-cd ../mangaUpdates
-pyinstaller executable.py --noconfirm --distpath ../../../appStarter/mangaInfoAPIs/mangaUpdates  --workpath ../../../appStarter/mangaInfoAPIs/mangaUpdates/build
-cd ..
-cd ..
-
-cd mangaAPIs
-cd manganelo
-pyinstaller executable.py --noconfirm --distpath ../../../appStarter/mangaAPIs/manganelo  --workpath ../../../appStarter/mangaAPIs/mangakakalottv/build
-cd ../mangakakalottv
-pyinstaller executable.py --noconfirm --distpath ../../../appStarter/mangaAPIs/mangakakalottv  --workpath ../../../appStarter/mangaAPIs/mangakakalottv/build
-cd ../mangakakalotcom
-pyinstaller executable.py --noconfirm --distpath ../../../appStarter/mangaAPIs/mangakakalotcom  --workpath ../../../appStarter/mangaAPIs/mangakakalotcom/build
-cd ..
-cd ..
-cd ..
-cd appStarter
-pyinstaller appStarter.py --noconfirm --add-data "mainAPI;mainAPI" --add-data "mangaAPIs;mangaAPIs" --add-data "mangaInfoAPIs;mangaInfoAPIs" --add-data "logo.png;."
-pause
-exit
+CD %root%\sourceAPIs
+rem https://stackoverflow.com/questions/12118810/arithmetic-inside-a-for-loop-batch-file
+setlocal enableDelayedExpansion
+FOR /F %%a IN ('dir /b /a:d "mangaInfoAPIs"') DO IF EXIST "%root%\sourceAPIs\mangaInfoAPIs\%%a\executable.py" (
+CD %root%\sourceAPIs\mangaInfoAPIs\%%a
+ECHO "Compiling %%a..."
+pyinstaller executable.py --noconfirm --distpath %appstarter%\mangaInfoAPIs\%%a --workpath %appstarter%\%%a_BUILD
+CD ..
+)
+FOR /F %%a IN ('dir /b /a:d "mangaAPIs"') DO IF EXIST "%root%\sourceAPIs\mangaAPIs\%%a\executable.py" (
+CD %root%\sourceAPIs\mangaInfoAPIs\%%a
+ECHO "Compiling %%a..."
+pyinstaller executable.py --noconfirm --distpath %appstarter%\mangaAPIs\%%a --workpath %appstarter%\%%a_BUILD
+CD ..
+)
+CD %appstarter%
+ECHO "Compiling appStarter..."
+pyinstaller appStarter.py --noconfirm --add-data "mainAPI;mainAPI" --add-data "mangaAPIs;mangaAPIs" --add-data "mangaInfoAPIs;mangaInfoAPIs" --add-data "logo.png;." --workpath %appstarter%\mainAPI_BUILD
+PAUSE

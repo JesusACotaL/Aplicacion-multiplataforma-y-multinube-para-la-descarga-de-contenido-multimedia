e1eeec3e3624ad20b4f49be32f39d8e0ce46d412
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-08-23 15:19:15 -0600

    Fix compilador: habia muchos errores de cd jajaj

--- mangaback/backendAPIs/compileAppStarter.cmd
+++ mangaback/backendAPIs/compileAppStarter.cmd
@@ -4,25 +4,24 @@ SET root=%CD%
 SET appstarter=%CD%\appStarter
 
 CD %root%\mainAPI
-ECHO "Compiling mainAPI..."
-pyinstaller executable.py --noconfirm --add-data "mangafront;mangafront" --add-data "firebase-credentials.json;." --distpath ../appStarter/mainAPI  --workpath ../appStarter/mainAPI/build
+ECHO ====================Compiling MainAPI...====================
+pyinstaller executable.py --noconsole --noconfirm --add-data "mangafront;mangafront" --add-data "firebase-credentials.json;." --distpath %appstarter%\mainAPI  --workpath %appstarter%\mainAPI_BUILD
 
 CD %root%\sourceAPIs
-rem https://stackoverflow.com/questions/12118810/arithmetic-inside-a-for-loop-batch-file
-setlocal enableDelayedExpansion
 FOR /F %%a IN ('dir /b /a:d "mangaInfoAPIs"') DO IF EXIST "%root%\sourceAPIs\mangaInfoAPIs\%%a\executable.py" (
 CD %root%\sourceAPIs\mangaInfoAPIs\%%a
-ECHO "Compiling %%a..."
-pyinstaller executable.py --noconfirm --distpath %appstarter%\mangaInfoAPIs\%%a --workpath %appstarter%\%%a_BUILD
-CD ..
+ECHO ====================Compiling %%a...====================
+pyinstaller executable.py --noconsole --noconfirm --distpath %appstarter%\mangaInfoAPIs\%%a --workpath %appstarter%\%%a_BUILD
 )
+
+CD %root%\sourceAPIs
 FOR /F %%a IN ('dir /b /a:d "mangaAPIs"') DO IF EXIST "%root%\sourceAPIs\mangaAPIs\%%a\executable.py" (
-CD %root%\sourceAPIs\mangaInfoAPIs\%%a
-ECHO "Compiling %%a..."
-pyinstaller executable.py --noconfirm --distpath %appstarter%\mangaAPIs\%%a --workpath %appstarter%\%%a_BUILD
-CD ..
+CD %root%\sourceAPIs\mangaAPIs\%%a
+ECHO ====================Compiling %%a...====================
+pyinstaller executable.py --noconsole --noconfirm --distpath %appstarter%\mangaAPIs\%%a --workpath %appstarter%\%%a_BUILD
 )
+
 CD %appstarter%
-ECHO "Compiling appStarter..."
-pyinstaller appStarter.py --noconfirm --add-data "mainAPI;mainAPI" --add-data "mangaAPIs;mangaAPIs" --add-data "mangaInfoAPIs;mangaInfoAPIs" --add-data "logo.png;." --workpath %appstarter%\mainAPI_BUILD
+ECHO ====================Compiling AppStarter...====================
+pyinstaller appStarter.py --noconsole --noconfirm --add-data "mainAPI;mainAPI" --add-data "mangaAPIs;mangaAPIs" --add-data "mangaInfoAPIs;mangaInfoAPIs" --add-data "logo.png;."
 PAUSE

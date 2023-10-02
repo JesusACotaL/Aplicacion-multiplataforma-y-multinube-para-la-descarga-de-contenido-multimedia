a26caf70c040e893c9cbbb68d494ac76fc8c1599
M. Mathier Mendoza Morelos <43389808+DeadZombie14@users.noreply.github.com>
2023-10-01 19:39:30 -0600

    Always use same dependencies version
    Stupid pyinstaller decided to release a crazy mayor version last week, breaking the way EVERYTHING works.
    
    So, leason learned: we now keep libraries version.

--- mangaback/backendAPIs/compileAppStarter.cmd
+++ mangaback/backendAPIs/compileAppStarter.cmd
@@ -5,23 +5,23 @@ SET appstarter=%CD%\appStarter
 
 CD %root%\mainAPI
 ECHO ====================Compiling MainAPI...====================
-pyinstaller executable.py --noconsole --noconfirm --add-data "mangafront;mangafront" --add-data "firebase-credentials.json;." --distpath %appstarter%\mainAPI  --workpath %appstarter%\mainAPI_BUILD
+pyinstaller executable.py --noconfirm --add-data "mangafront;mangafront" --add-data "firebase-credentials.json;." --distpath %appstarter%\mainAPI  --workpath %appstarter%\mainAPI_BUILD
 
 CD %root%\sourceAPIs
 FOR /F %%a IN ('dir /b /a:d "mangaInfoAPIs"') DO IF EXIST "%root%\sourceAPIs\mangaInfoAPIs\%%a\executable.py" (
 CD %root%\sourceAPIs\mangaInfoAPIs\%%a
 ECHO ====================Compiling %%a...====================
-pyinstaller executable.py --noconsole --noconfirm --distpath %appstarter%\mangaInfoAPIs\%%a --workpath %appstarter%\%%a_BUILD
+pyinstaller executable.py --noconfirm --distpath %appstarter%\mangaInfoAPIs\%%a --workpath %appstarter%\%%a_BUILD
 )
 
 CD %root%\sourceAPIs
 FOR /F %%a IN ('dir /b /a:d "mangaAPIs"') DO IF EXIST "%root%\sourceAPIs\mangaAPIs\%%a\executable.py" (
 CD %root%\sourceAPIs\mangaAPIs\%%a
 ECHO ====================Compiling %%a...====================
-pyinstaller executable.py --noconsole --noconfirm --distpath %appstarter%\mangaAPIs\%%a --workpath %appstarter%\%%a_BUILD
+pyinstaller executable.py --noconfirm --distpath %appstarter%\mangaAPIs\%%a --workpath %appstarter%\%%a_BUILD
 )
 
 CD %appstarter%
 ECHO ====================Compiling AppStarter...====================
-pyinstaller appStarter.py --noconsole --noconfirm --add-data "mainAPI;mainAPI" --add-data "mangaAPIs;mangaAPIs" --add-data "mangaInfoAPIs;mangaInfoAPIs" --add-data "logo.png;."
+pyinstaller appStarter.py --noconfirm --add-data "mainAPI;mainAPI" --add-data "mangaAPIs;mangaAPIs" --add-data "mangaInfoAPIs;mangaInfoAPIs" --add-data "logo.png;." --workpath %appstarter%\appstarter_BUILD
 PAUSE
